39797077748a6dd7ba4c2b2a165d0bbca23e8d88
aghog <aghog@192.168.2.116>
2025-03-28 17:28:07 -0400

    Comitting room 3

---
+++ EscapeRoom/src/com/escaperoom/room2/Item.java
@@ -0,0 +1,11 @@
+package com.escaperoom.room2;
+
+import java.io.FileReader;
+import javax.media.j3d.*;
+import org.jogamp.java3d.TransformGroup;
+
+
+public class Item {
+	private String name;
+	private TransformGroup transformGroup;
+}
