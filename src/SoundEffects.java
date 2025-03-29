c7769e79dcdce4e62c20f5476d4d25880516d65e
aghog <aghog@192.168.2.116>
2025-03-29 18:52:27 -0400

    Sound File

--- src/SoundEffects.java
+++ src/SoundEffects.java
@@ -1,6 +1,4 @@
 import java.net.URL;
-import org.jdesktop.j3d.examples.sound.BackgroundSoundBehavior;
-import org.jdesktop.j3d.examples.sound.audio.JOALMixer;
 import org.jogamp.java3d.BackgroundSound;
 import org.jogamp.java3d.BoundingSphere;
 import org.jogamp.java3d.MediaContainer;
@@ -13,5 +11,56 @@ import java.awt.event.ActionListener;
 import java.awt.event.ActionEvent;
 
 public class SoundEffects {
+	 public static PointSound horrorBGSound = createSound("sounds/(Free) Horror Ambiance - Ominous Background Music.wav");
+	 public static PointSound doorBGSound = createSound("sounds/DOOR Opening Sound Effect ( HD ) Copyright Free.wav");
+	 public static PointSound correctPinBGSound = createSound("sounds/Success Sound Effect.wav");
+	 public static PointSound wrongPinBGSound = createSound("sounds/Error - Sound Effect ｜ Non copyright sound effects ｜ FeeSou.wav");
+	 public static PointSound heartbeatBGSound = createSound("sounds/Dramatic Heartbeat Sound Effect.wav");
+	 
+	 public static void playSound(PointSound sound) {
+	        sound.setEnable(true);
+	        Timer timer = new Timer(2000, new ActionListener() {
+	            public void actionPerformed(ActionEvent e) {
+	                sound.setEnable(false);
+	                ((Timer) e.getSource()).stop();
+	            }
+	        });
+	        timer.setRepeats(false);
+	        timer.start();
+	    }
+	 
+	 public static PointSound createSound(String name) {
+		 URL soundURL = locateSound(name);
+	        if (soundURL == null) {
+	            System.err.println("Error: Sound file not found: " + name);
+	            return null;
+	        }
+	        MediaContainer pointMedia = new MediaContainer(soundURL);
+	        pointMedia.setCacheEnable(true);
+
+	        PointSound pointSound = new PointSound();
+	        pointSound.setSoundData(pointMedia);
+	        pointSound.setEnable(false);
+	        pointSound.setInitialGain(0.5f);
+	        pointSound.setCapability(PointSound.ALLOW_ENABLE_WRITE);
+	        pointSound.setSchedulingBounds(new BoundingSphere(new Point3d(), 1000));
+	        return pointSound;
+    }
+	 
+	public static URL locateSound(String fn) {
+		return SoundEffects.class.getClassLoader().getResource("sounds/" + fn);
+
+	}
+	 
+	 public static BackgroundSound bkgdSound() {
+	     BackgroundSound bgSound = new BackgroundSound();
+		 bgSound.setSchedulingBounds(new BoundingSphere(new Point3d(), 1000));
+		 return bgSound;
+	 }
+	 
+	 public static void enableAudio(SimpleUniverse simple_U) {
+	        Viewer viewer = simple_U.getViewer();
+	        viewer.getView().setBackClipDistance(20.0f);
+	}
 
 }
