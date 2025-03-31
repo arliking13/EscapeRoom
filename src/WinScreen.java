c85e6cf3ffe45479b3c763fbab62ddbf7d402500
aghog <aghog@192.168.2.116>
2025-03-30 22:04:47 -0400

    Screen one error - pelumi

--- src/WinScreen.java
+++ src/WinScreen.java
@@ -1,16 +1,39 @@
-import java.awt.Color;
-import java.awt.Font;
-import java.awt.Graphics;
+import javax.swing.*;
+import java.awt.*;
+import javax.swing.border.EmptyBorder;
 
-public class WinScreen extends Screen {
+public class WinScreen extends JPanel {
+    /**
+	 * 
+	 */
 	private static final long serialVersionUID = 1L;
+	public WinScreen(JFrame frame) {
+        setLayout(new BorderLayout());
+        setBackground(Color.BLACK);
+        setBorder(new EmptyBorder(50, 30, 50, 30));
 
-	@Override
-	    public void paintComponent(Graphics g) {
-	        g.setColor(Color.BLACK);
-	        g.fillRect(0, 0, getWidth(), getHeight());
-	        g.setColor(Color.WHITE);
-	        g.setFont(new Font("Arial", Font.BOLD, 50));
-	        g.drawString("You escaped from the Haunted House this time!", getWidth() / 2 - 150, getHeight() / 2 + 50);
-	    }
+        // Main victory message
+        JLabel titleLabel = new JLabel("ESCAPE SUCCESSFUL", SwingConstants.CENTER);
+        titleLabel.setFont(new Font("Courier New", Font.BOLD, 48));
+        titleLabel.setForeground(new Color(0xE74C3C)); // Blood red
+        titleLabel.setBorder(BorderFactory.createEmptyBorder(20, 0, 40, 0));
+
+        // Subtext with typewriter effect
+        JTextArea subtext = new JTextArea();
+        subtext.setEditable(false);
+        subtext.setLineWrap(true);
+        subtext.setWrapStyleWord(true);
+        subtext.setBackground(Color.BLACK);
+        subtext.setForeground(Color.WHITE);
+        subtext.setFont(new Font("Courier New", Font.PLAIN, 18));
+        subtext.setText("The door creaks open as you stumble into the moonlight...\n\n" +
+                      "The nightmare is over. For now.");
+
+        // Add components
+        add(titleLabel, BorderLayout.NORTH);
+        add(subtext, BorderLayout.CENTER);
+
+        // Play victory sound (reuse your sound system)
+        SoundEffects.playSound(SoundEffects.correctPinBGSound);
+    }
 }
