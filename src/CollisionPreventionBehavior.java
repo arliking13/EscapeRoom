3a7fd9181ed58ef212627138a432eabc20bae836
aghog <aghog@192.168.2.116>
2025-04-04 16:19:24 -0400

    Collisions push again

--- src/CollisionPreventionBehavior.java
+++ src/CollisionPreventionBehavior.java
@@ -1,6 +1,9 @@
 import java.util.Iterator;
 import org.jogamp.java3d.Behavior;
 import org.jogamp.java3d.Shape3D;
+import org.jogamp.java3d.Transform3D;
+import org.jogamp.java3d.TransformGroup;
+import org.jogamp.java3d.TransparencyAttributes;
 import org.jogamp.java3d.WakeupCriterion;
 import org.jogamp.java3d.WakeupOnCollisionEntry;
 import org.jogamp.java3d.WakeupOnCollisionExit;
@@ -11,9 +14,13 @@ public class CollisionPreventionBehavior extends Behavior {
     private WakeupOr wakeupCondition;
     private boolean isColliding = false;
     private Shape3D playerShape;
+    private TransformGroup playerTG;
+    private Transform3D lastSafeTransform = new Transform3D();
 
-    public CollisionPreventionBehavior(Shape3D player) {
+    public CollisionPreventionBehavior(Shape3D player, TransformGroup tg) {
         this.playerShape = player;
+        this.playerTG = tg;
+        this.playerTG.getTransform(lastSafeTransform); // Store initial safe position
     }
 
     public void initialize() {
@@ -25,18 +32,26 @@ public class CollisionPreventionBehavior extends Behavior {
         wakeupOn(wakeupCondition);
     }
 
-    public void processStimulus(Iterator criteria) {
+    public void processStimulus(Iterator<WakeupCriterion> criteria) {
+    	 TransparencyAttributes transparencyAttributes = new TransparencyAttributes();
+         transparencyAttributes.setTransparencyMode(TransparencyAttributes.FASTEST);
         while (criteria.hasNext()) {
-            WakeupCriterion criterion = (WakeupCriterion) criteria.next();
+            WakeupCriterion criterion = criteria.next();
 
             if (criterion instanceof WakeupOnCollisionEntry) {
                 isColliding = true;
-                System.out.println("Collision detected!");
+                System.out.println("Collision detected! Reverting movement.");
+                playerTG.setTransform(lastSafeTransform); // Revert position
             } else if (criterion instanceof WakeupOnCollisionExit) {
                 isColliding = false;
-                System.out.println("No longer colliding.");
             }
         }
+
+        // Save the last safe position if not colliding
+        if (!isColliding) {
+            playerTG.getTransform(lastSafeTransform);
+        }
+
         wakeupOn(wakeupCondition);
     }
 
