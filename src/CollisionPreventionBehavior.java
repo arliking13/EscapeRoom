655d66b92066f6476fe9e2fe09263857b9c1dd90
rahiahmed <rahiahmed1819@gmail.com>
2025-04-04 11:31:49 -0400

    Instead of direct System.out.println, added debug toggling for CollisionPreventionBehavior

--- src/CollisionPreventionBehavior.java
+++ src/CollisionPreventionBehavior.java
@@ -7,41 +7,65 @@ import org.jogamp.java3d.WakeupOnCollisionExit;
 import org.jogamp.java3d.WakeupOr;
 
 public class CollisionPreventionBehavior extends Behavior {
-	private WakeupCriterion[] wakeupArray;
-    private WakeupOr wakeupCondition;
+    private final WakeupCriterion[] wakeupArray;
+    private final WakeupOr wakeupCondition;
     private boolean isColliding = false;
-    private Shape3D playerShape;
+    private final Shape3D playerShape;
+    private boolean debug = false;
 
     public CollisionPreventionBehavior(Shape3D player) {
+        if (player == null) {
+            throw new IllegalArgumentException("Player shape cannot be null");
+        }
         this.playerShape = player;
-    }
-
-    public void initialize() {
         wakeupArray = new WakeupCriterion[]{
             new WakeupOnCollisionEntry(playerShape, WakeupOnCollisionEntry.USE_GEOMETRY),
             new WakeupOnCollisionExit(playerShape, WakeupOnCollisionExit.USE_GEOMETRY)
         };
         wakeupCondition = new WakeupOr(wakeupArray);
+    }
+
+    public void setDebug(boolean debug) {
+        this.debug = debug;
+    }
+
+    @Override
+    public void initialize() {
         wakeupOn(wakeupCondition);
     }
 
-    public void processStimulus(Iterator criteria) {
+    @Override
+    public void processStimulus(Iterator<WakeupCriterion> criteria) {
         while (criteria.hasNext()) {
-            WakeupCriterion criterion = (WakeupCriterion) criteria.next();
+            WakeupCriterion criterion = criteria.next();
 
             if (criterion instanceof WakeupOnCollisionEntry) {
                 isColliding = true;
-                System.out.println("Collision detected!");
+                onCollisionEnter();
             } else if (criterion instanceof WakeupOnCollisionExit) {
                 isColliding = false;
-                System.out.println("No longer colliding.");
+                onCollisionExit();
             }
         }
         wakeupOn(wakeupCondition);
     }
 
+    protected void onCollisionEnter() {
+        if (debug) System.out.println("Collision detected!");
+    }
+
+    protected void onCollisionExit() {
+        if (debug) System.out.println("No longer colliding.");
+    }
+
     public boolean isColliding() {
         return isColliding;
     }
+
+    @Override
+    public String toString() {
+        return "CollisionPreventionBehavior for: " + playerShape.getName();
+    }
 }
+
 
